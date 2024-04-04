f23e266484fee1e78056f438a8eb74f79032e637
Hernán García Quijano <hernangarqui@gmail.com>
2024-04-04 21:46:09 +0100

    propietarioController moved out

--- Backend/src/main/java/com/group8/diy4rent/Controller/ClienteController.java
+++ Backend/src/main/java/com/group8/diy4rent/Controller/ClienteController.java
@@ -36,71 +36,55 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @CrossOrigin
 public class ClienteController {
 
-    private final ClienteRepository clienteRepository;
-    private final PropietarioRepository propietarioRepository;
+  private final ClienteRepository clienteRepository;
+  private final PropietarioRepository propietarioRepository;
 
-    public ClienteController(ClienteRepository clienteRepository, PropietarioRepository propietarioRepository) {
-        this.clienteRepository = clienteRepository;
-        this.propietarioRepository = propietarioRepository;
-    }
+  public ClienteController(ClienteRepository clienteRepository, PropietarioRepository propietarioRepository) {
+    this.clienteRepository = clienteRepository;
+    this.propietarioRepository = propietarioRepository;
+  }
 
-    // Primero, los de acceso permitido a clientes normales y corrientes
+  // Primero, los de acceso permitido a clientes normales y corrientes
 
-    @GetMapping("/clientes/{id}")
+  @GetMapping("/clientes/{id}")
 	ResponseEntity<Cliente> getCliente(@PathVariable Integer id) {
 	// return (List<Cliente>) clienteRepository.findBynombre("hernan");
 	return clienteRepository.findById(id).map(c -> ResponseEntity.ok().body(c))
-            .orElse(new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND));
-
+    .orElse(new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND));
 	}
 
-    // Para que el cliente pueda actualizar sus datos
-    @PutMapping("/clientes/{id}")
+  // Para que el cliente pueda actualizar sus datos
+  @PutMapping("/clientes/{id}")
 	public ResponseEntity<Cliente> actualizarCliente (@RequestBody Cliente cliente, @PathVariable Integer id) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-  
-        return clienteRepository.findById(id).map(c -> {
-        
-        cliente.setNombre(cliente.getNombre());
-        cliente.setApellidos(cliente.getApellidos());
-        cliente.setEmail(cliente.getEmail());
-        cliente.setLatitud(cliente.getLatitud());
-        cliente.setLongitud(cliente.getLongitud());
-        cliente.setIban(cliente.getIban());
-        clienteRepository.save(cliente);
-        return ResponseEntity.ok().body(cliente);
-        })
-        .orElse(new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND));
-      }
-
-      @PostMapping("/clientes")
-      ResponseEntity<Cliente> añadirCliente(@RequestBody Cliente newCliente) throws URISyntaxException {
-      Cliente result = clienteRepository.save(newCliente);
-      return ResponseEntity.created(new URI("/clientes/" + result.getNombre() + result.getApellidos())).body(result);
-      }
-
-      @PostMapping("/propietarios")
-      ResponseEntity<Propietario> añadirPropietario(@RequestBody Cliente newCliente) throws URISyntaxException {
-      Propietario propietario = new Propietario();
-      propietario.setId(newCliente.getId());
-      TipoClienteEnum tipo = newCliente.getTipo();
-      if (tipo.equals(TipoClienteEnum.PROPIETARIO)){
-        propietario.setId(newCliente.getId());
-        Propietario result = propietarioRepository.save(propietario);
-        return ResponseEntity.created(new URI("/propietarios/" + propietario.getId())).body(result);
-      } else {
-        // En caso de que el tipo de cliente no sea "PROPIETARIO", puedes devolver un ResponseEntity con un estado de error
-        return ResponseEntity.badRequest().build();
+    // @ResponseBody means the returned String is the response, not a view name
+    // @RequestParam means it is a parameter from the GET or POST request
+    return clienteRepository.findById(id).map(c -> {          
+    cliente.setNombre(cliente.getNombre());
+    cliente.setApellidos(cliente.getApellidos());
+    cliente.setEmail(cliente.getEmail());
+    cliente.setLatitud(cliente.getLatitud());
+    cliente.setLongitud(cliente.getLongitud());
+    cliente.setIban(cliente.getIban());
+    clienteRepository.save(cliente);
+    return ResponseEntity.ok().body(cliente);
+    })
+    .orElse(new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND));
     }
-      }
 
+  @PostMapping("/clientes")
+  ResponseEntity<Cliente> añadirCliente(@RequestBody Cliente newCliente) throws URISyntaxException {
+    Cliente result = clienteRepository.save(newCliente);
+    return ResponseEntity.created(new URI("/clientes/" + result.getNombre() + result.getApellidos())).body(result);
+  }
 
-      // Para admins
-      @GetMapping("/clientes")
-      List<Cliente> getClientes() {
-      // return (List<Cliente>) clienteRepository.findBynombre("hernan");
-      return clienteRepository.findAll();
   
-      }
+
+
+  // Para admins
+  @GetMapping("/clientes")
+  List<Cliente> getClientes() {
+  // return (List<Cliente>) clienteRepository.findBynombre("hernan");
+  return clienteRepository.findAll();
+  
+  }
 }
