a18ec6893f4b15252aab50c02006ebfdf39254ce
pablobote <p.bote@alumnos.upm.es>
2024-04-06 20:10:12 +0200

    mejora gráfica

--- frontend/src/main/Navbar.js
+++ frontend/src/main/Navbar.js
@@ -7,50 +7,40 @@ import axios from 'axios';
 
 
 function Navbar(props) {
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    handleClick();
+  }, []);
 
-const [input, setInput] = useState('');
-const [items, setItems] = useState([]);
-
-  
-    // const [lista, setLista] = useState(response.data);
-    
-    // function filtrarInput(items, palabra) {
-    //   let res = [];
-    //       res = (items.filter((producto) =>
-    //        producto.nombre.toLowerCase().includes(palabra.toLowerCase())));
-    //   props.onFilterChange(res);
-    // }
-
-    useEffect(() => {
-      handleClick()
-    }, []);
-
-    function handleClick (){
-      props.onFilterChange(props.herramientas, input);
-    }
-
-    return (
-      <nav className="navbar" style={{ marginBottom: '20px' }}>
-        <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
-          <li style={{ display: 'flex', alignItems: 'center' }}>
-            <img src={logo} alt="Logo" style={{ width: '75px', height: '75px', marginRight: '10px' }} /> {/* Cambia la ruta y el nombre del archivo según corresponda */}
-            <Link className="letras" to="/" href="#" style={{ fontSize: '24px'}}>DIY4RENT</Link>
-          </li>
-          <div>
-            <input id="filtro" onChange={e => setInput(e.target.value)}></input>
-            <button onClick={handleClick} style={{ marginTop: '10px', backgroundColor: '#606C38', marginLeft: '500px', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Buscar</a></button>
-          </div>
-          <Link className="letras" to="/publicar" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a className="letras" href="#" >Publica tu herramienta</a></Link>
-          <Link className="letras" to="/mytools" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a className="letras" href="#" >Mis herramientas</a></Link>
-          <Link className="letras" to="/ayuda" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a className="letras" href="#" >Ayuda</a></Link>
-          <Link className="letras" to ="/registrar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px' }}><a className="letras" href="#" >Registrarse</a></Link>
-          <Link className="letras" to="/iniciar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px' }}><a className="letras" href="#" >Iniciar sesión</a></Link>
-        </ul>
-      </nav>
-      
-    );
+  function handleClick() {
+    props.onFilterChange(props.herramientas, input);
   }
+
+  return (
+    <nav className="navbar" style={{ marginBottom: '20px' }}>
+      <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
+        <li style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={logo} alt="Logo" style={{ width: '75px', height: '75px', marginRight: '10px' }} />
+          <Link className="letras" to="/" href="#" style={{ fontSize: '24px' }}><strong style={{ color: 'white' }}>DIY4RENT</strong></Link>
+        </li>
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: '20px' }}>
+          <input id="filtro" onChange={e => setInput(e.target.value)} style={{ marginRight: '10px', height: '30px', fontSize: '18px' }}></input>
+          <button onClick={handleClick} style={{ backgroundColor: '#606C38', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Buscar</a></button>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center'}}>
+        <Link className="letras" to="/publicar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px', fontSize: '16px' }}><a className="letras" href="#"><strong style={{ color: 'white' }}>Publica tu herramienta</strong></a></Link>
+        <Link className="letras" to="/mytools" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px', fontSize: '16px' }}><a className="letras" href="#"><strong style={{ color: 'white' }}>Mis herramientas</strong></a></Link>
+        <Link className="letras" to="/ayuda" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px', fontSize: '16px' }}><a className="letras" href="#"><strong style={{ color: 'white' }}>Ayuda</strong></a></Link>
+        <Link className="letras" to="/registrar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px', fontSize: '16px' }}><a className="letras" href="#"><strong style={{ color: 'white' }}>Registrarse</strong></a></Link>
+        <Link className="letras" to="/iniciar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px', fontSize: '16px' }}><a className="letras" href="#"><strong style={{ color: 'white' }}>Iniciar sesión</strong></a></Link>
+        </div>
+       
+      </ul>
+    </nav>
+  );
+}
 
 
 export default Navbar;
