c72a0089bf08fec17751256b9a7e8434fe199853
pablobote <p.bote@alumnos.upm.es>
2024-04-06 18:36:14 +0200

    cambios.

--- frontend/src/main/Navbar.js
+++ frontend/src/main/Navbar.js
@@ -35,19 +35,18 @@ const [items, setItems] = useState([]);
         <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
           <li style={{ display: 'flex', alignItems: 'center' }}>
             <img src={logo} alt="Logo" style={{ width: '75px', height: '75px', marginRight: '10px' }} /> {/* Cambia la ruta y el nombre del archivo según corresponda */}
-            <Link to="/" href="#" style={{ fontSize: '24px', color: 'black' }}>DIY4RENT</Link>
+            <Link className="letras" to="/" href="#" style={{ fontSize: '24px'}}>DIY4RENT</Link>
           </li>
           <div>
             <input id="filtro" onChange={e => setInput(e.target.value)}></input>
-            <button onClick={handleClick} style={{ marginTop: '10px', marginLeft: '500px', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Buscar</a></button>
+            <button onClick={handleClick} style={{ marginTop: '10px', backgroundColor: '#606C38', marginLeft: '500px', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Buscar</a></button>
           </div>
-          <Link to="/publicar" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Publica tu herramienta</a></Link>
-          <Link to="/mytools" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Mis herramientas</a></Link>
-          <Link to="/ayuda" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a href="#" style={{ color: 'black' }}>Ayuda</a></Link>
-          <Link to ="/registrar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid black', padding: '5px' }}><a href="#" style={{ color: 'black' }}>Registrarse</a></Link>
-          <Link to="/iniciar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid black', padding: '5px' }}><a href="#" style={{ color: 'black' }}>Iniciar sesión</a></Link>
+          <Link className="letras" to="/publicar" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a className="letras" href="#" >Publica tu herramienta</a></Link>
+          <Link className="letras" to="/mytools" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a className="letras" href="#" >Mis herramientas</a></Link>
+          <Link className="letras" to="/ayuda" href="#" style={{ marginTop: '10px', marginLeft: '50px', display: 'flex', alignItems: 'center' }}><a className="letras" href="#" >Ayuda</a></Link>
+          <Link className="letras" to ="/registrar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px' }}><a className="letras" href="#" >Registrarse</a></Link>
+          <Link className="letras" to="/iniciar" href="#" style={{ marginTop: '20px', marginLeft: '50px', display: 'flex', alignItems: 'center', borderRadius: '10px', border: '2px solid #FEFAE0', padding: '5px' }}><a className="letras" href="#" >Iniciar sesión</a></Link>
         </ul>
-        
       </nav>
       
     );
