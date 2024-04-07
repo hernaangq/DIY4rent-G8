8eb854315d20ddfe7401dc5aa6a96159d7c1b411
Hernán García Quijano <hernangarqui@gmail.com>
2024-04-07 20:44:58 +0100

    cmabios en tool.js

--- frontend/src/herramienta/Tool.js
+++ frontend/src/herramienta/Tool.js
@@ -8,7 +8,7 @@ const Tool = (props) => {
 
   let { rutaId } = useParams();
   let herramienta = props.herramientas[rutaId];
-  console.log(herramienta)
+  //console.log(herramienta)
   let herramientaId = herramienta.id;
 
   const handleAlquilarClick = async () => {
@@ -20,8 +20,8 @@ const Tool = (props) => {
     let id = 1; // Cambiar por el id del usuario actual
     let response = await axios.post('http://localhost:8443/alquileres/' + id + '/' + herramientaId, {});
     let respuesta = await axios.patch('http://localhost:8443/herramientas/' + herramientaId, {estaAlquilada: true});
-    console.log(respuesta);
-
+    //console.log(respuesta);
+    
     // const datos = await response.json();
   }
 
@@ -47,10 +47,11 @@ const Tool = (props) => {
     //console.log(response2.data);
   }
 
-  const estrellasNum = alquileres.reduce((acc, alquiler) => {
+  var estrellasNum = alquileres.reduce((acc, alquiler) => {
     return acc + alquiler.estrellasServicio
   }, 0) / alquileres.length;
 
+  estrellasNum = (Math.round(estrellasNum * 100) / 100).toFixed(2);
   
   const renderEmojis = () => {
     const stars = [];
@@ -60,16 +61,35 @@ const Tool = (props) => {
     return stars;
   };
 
-  console.log(renderEmojis());
+  //console.log(renderEmojis());
   
   var rawResponse = herramienta.foto; 
-  console.log(rawResponse);
+  
+
+  const valoraciones = alquileres.map(alquiler => {
+    return (
+      <div>
+        <p>{alquiler.usuario.nombre} {alquiler.usuario.apellidos}: {alquiler.valoracion}</p> 
+      </div>
+    );
+  });
+
+  console.log(valoraciones);
 
   return (
     <div className="container">
       <div className="tool-image" style={{float: 'left' }}>
-        <img src={`data:image/jpg;base64, ${rawResponse}`} alt="Tool" style={{  height: '450px', width: '350px', height: 'auto' }} />
+        <img src={`data:image/jpg;base64, ${rawResponse}`} alt="Tool" style={{  height: '550px', width: '450px', height: 'auto' }} />
+        <div className="ratings">
+          <h3>Ratings</h3>
+          <p>{renderEmojis()}   {estrellasNum} estrellas</p>
+        </div>
+        <div className="valoracion">
+          <h3>Valoraciones</h3>
+          <div>{valoraciones}</div>
+        </div>
       </div>
+      
       <div className="tool-details" style={{ maxWidth: '100%', width: '800px' }}>
         <div className="tool-info">
           <h2>{herramienta.nombre}</h2>
@@ -78,15 +98,12 @@ const Tool = (props) => {
         <div className="alquila">
           <button onClick={handleAlquilarClick}>Alquílalo</button>
         </div>
-        <div className="ratings">
-          <h3>Ratings</h3>
-          <p>{renderEmojis()}   {estrellasNum} estrellas</p>
-        </div>
+
         <div className="location">
-          <h3>Available Locations</h3>
+          <h3>Localización</h3>
         </div>
         <div class="google-map">
-        <iframe src={`https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d341488.5704903917!2d${herramienta.propietario.longitud}!3d${herramienta.propietario.latitud}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDIzJzA2LjQiTiAywrAxMCcyNC4yIkU!5e0!3m2!1sen!2ses!4v1712511678535!5m2!1sen!2ses`} width="500" height="350" style={{border:'1'}} allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src={`https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d341488.5704903917!2d${herramienta.propietario.longitud}!3d${herramienta.propietario.latitud}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDIzJzA2LjQiTiAywrAxMCcyNC4yIkU!5e0!3m2!1sen!2ses!4v1712511678535!5m2!1sen!2ses`} width="10" height="20" style={{border:'1'}} allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
